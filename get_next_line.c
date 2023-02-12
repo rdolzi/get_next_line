13d6679a0d22528421845c6d50ac6a30ced10520
roger-rab-bit <ruggero-dolzi@virgilio.it>
2023-02-12 21:18:35 +0100

    Bug fix

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rdolzi <rdolzi@student.42roma.it>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 12:12:48 by rdolzi            #+#    #+#             */
-/*   Updated: 2023/02/12 18:12:10 by rdolzi           ###   ########.fr       */
+/*   Updated: 2023/02/12 21:15:35 by rdolzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,13 @@
 // utilizza read per leggere il file e salva in backup (grandezza BUFFER_SIZE)
 	//leggere buffer volte fino a che non troviamo non raggiunge \n o EOF
 	//salvare buffer in stack e continuare a leggere
+	// || len != 0 ? 
 char	*ft_read(int fd, char *stack)
 {
 	char	*buffer;
 	int		len;
 
-	while (len != 0)
+	while (!ft_strchr(stack, '\n'))
 	{
 		buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 		if (!buffer)
@@ -35,12 +36,32 @@ char	*ft_read(int fd, char *stack)
 	}
 	return (stack);
 }
-/*
-// estrae la linea dallo stack
+
+// lo stack contiene lo \n ma può contenere anche char successivi
+// extract_line estrae la linea dallo stack ( \n incluso ) 
+// e aggiunge il null byte
 char	*extract_line(char *stack)
 {
-}
+	size_t	i;
+	char	*line;
 
+	i = 0;
+	while (stack[i] != '\n')
+		i++;
+	line = (char *)malloc(sizeof(char) * (i + 2));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (stack[i] != '\n')
+	{
+		line[i] = stack[i];
+		i++;
+	}
+	line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+/*
 //rimuove la la prima linea(tutti gli elementi fino a \n)
 void	ft_clean(char *stack)
 {
@@ -57,16 +78,17 @@ char	*get_next_line(int fd)
 		return (NULL);
 	stack = ft_read(fd, stack);
 	if (!stack)
-		return (NULL);/*
+		return (NULL);
 	line = extract_line(stack);
 	if (!line)
-		return (NULL);
+		return (NULL);/*
 	ft_clean(stack);*/
-	return (stack);
+	return (line); //stack
 }
 
 int main()
 {
 	int fd = open("test.txt", O_RDONLY);
+	printf("%s",get_next_line(fd));
 	printf("%s",get_next_line(fd));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rdolzi <rdolzi@student.42roma.it>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 11:27:09 by rdolzi            #+#    #+#             */
-/*   Updated: 2023/02/12 18:05:38 by rdolzi           ###   ########.fr       */
+/*   Updated: 2023/02/12 20:31:57 by rdolzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char *s1, char *s2);
-int     ft_strchr(char *s1, char c);
+int		ft_strchr(char *s1, char c);
 
 #endif
