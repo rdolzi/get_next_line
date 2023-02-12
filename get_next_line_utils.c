5deec7176330a4ffc33f67d1a32f71f33a2d33ad
roger-rab-bit <ruggero-dolzi@virgilio.it>
2023-02-12 18:13:44 +0100

    Add file

---
+++ get_next_line_utils.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdolzi <rdolzi@student.42roma.it>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/12 16:40:04 by rdolzi            #+#    #+#             */
+/*   Updated: 2023/02/12 18:08:29 by rdolzi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	ft_strchr(char *s1, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s1[i])
+	{
+		if (s1[i++] == c)
+			return (1);
+	}
+	return (0);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	s1len;
+	size_t	s2len;
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	s1len = ft_strlen(s1);
+	s2len = ft_strlen(s2);
+	str = (char *)malloc (s1len + s2len + 1);
+	if (!str)
+		return (NULL);
+	while (i < s1len)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (j < s2len)
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	return (str);
+}
